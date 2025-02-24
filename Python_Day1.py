e2cc1733a14ed8a9c72923d4845fb24e3abb16ab
ABC <mayukc55@gmail.com>
2025-02-24 18:59:56 +0530

    Day 1

---
+++ Python_Day1.py
@@ -0,0 +1,17 @@
+print("hello World")
+
+# This is our Day 1
+
+
+# Introduction
+# Variable Datatype Operators(Assignment,Relational,Logical,Arithmatic) Literals
+# Conditions (if , else, elif)
+# Loops(for,while)
+# Array(List,Tuple,Dictionary)
+# Functions
+# Files
+#
+# Backend
+# (Django ------>Html Concept, Python Concept)
+
+
